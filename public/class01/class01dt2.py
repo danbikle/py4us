3e0abf5add790e26ad5cdbb59670896044ebcf11
Dan Bikle <bikle@bikle.com>
2016-12-04 18:19:36 -0800

    more

---
+++ public/class01/class01dt2.py
@@ -0,0 +1,17 @@
+# class01dt2.py
+
+# This script should Copy Values Between Some Common Data Types
+
+# Demo:
+# python class01dt2.py
+
+import pdb
+# I should run this script in the debugger:
+pdb.set_trace()
+
+# I should copy 1 from Integer to Float:
+my_i = 1
+my_f = float(my_i)
+print(my_f)
+
+'bye'
