41f00d20ce590923a28bab5caee5b34aba437c9b
Dan Bikle <bikle@bikle.com>
2016-12-26 12:42:23 -0800

    more

--- public/class03/ibm1.py
+++ public/class03/ibm1.py
@@ -11,6 +11,6 @@ import requests
 entire_page_rq = requests.get('https://finance.yahoo.com/quote/IBM')
 entire_page_s  = entire_page_rq.text
 print('Some text from https://finance.yahoo.com/quote/IBM is displayed below:')
-print(entire_page_s[0:22])
+print(entire_page_s[0:100])
 
 'bye'
