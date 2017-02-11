03880b0a240cb270188998120c9244a6106f8d0c
Dan Bikle <bikle@bikle.com>
2017-02-11 00:03:57 -0800

    more

--- public/class06/ch32.py
+++ public/class06/ch32.py
@@ -13,7 +13,7 @@ from bokeh.sampledata.autompg import autompg as df
 p = Scatter(df, x='mpg', y='hp', color='cyl', title="HP vs MPG (shaded by CYL)",
             xlabel="Miles Per Gallon", ylabel="Horsepower")
 
-output_file("scatter.html")
+output_file("/tmp/ch32.html")
 
 show(p)
 
