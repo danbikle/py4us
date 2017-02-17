8c5fb5618d3275c6cfd39b88690863b40fb862ea
Dan Bikle <bikle@bikle.com>
2017-02-16 23:12:47 -0800

    more

---
+++ public/class07/sb15.py
@@ -0,0 +1,16 @@
+# sb15.py
+
+# Ref:
+# https://seaborn.pydata.org/tutorial/aesthetics.html#removing-spines-with-despine
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+data = np.random.normal(size=(20, 6)) + np.arange(6) / 2
+sns.set_style("whitegrid")
+sns.boxplot(data=data, palette="deep")
+sns.despine(left=True)
+plt.show()
+'bye'
