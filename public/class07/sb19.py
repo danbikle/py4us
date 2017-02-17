f7ce080ee59bc23ae13265745541f70fb841ff07
Dan Bikle <bikle@bikle.com>
2017-02-16 23:48:54 -0800

    more

--- public/class07/sb19.py
+++ public/class07/sb19.py
@@ -1,7 +1,7 @@
-# sb17.py
+# sb19.py
 
 # Ref:
-# https://seaborn.pydata.org/tutorial/aesthetics.html#overriding-elements-of-the-seaborn-styles
+# https://seaborn.pydata.org/tutorial/aesthetics.html#scaling-plot-elements-with-plotting-context-and-set-context
 
 import seaborn as sns
 
@@ -9,13 +9,14 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-print(sns.axes_style())
-
 def sinplot(flip=1):
     x = np.linspace(0, 14, 100)
     for i in range(1, 7):
         plt.plot(x, np.sin(x + i * .5) * (7 - i) * flip)
 
-sns.set_style("darkgrid", {"axes.facecolor": ".9", 'grid.color': 'red'})
+sns.set()
+sns.set_style("whitegrid")
+sns.set_context("poster", font_scale=4.5, rc={"lines.linewidth": 9.5})
+plt.figure(figsize=(16, 12))
 sinplot()
 plt.show()
