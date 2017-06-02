cacfe06e8619d8e51fabaf7dc0cbb530306afd1c
Dan Bikle <bikle@bikle.com>
2017-06-01 22:52:50 -0700

    more

--- public/class01/pdr1.py
+++ public/class01/pdr1.py
@@ -3,14 +3,25 @@ pdr1.py
 This script should use pandas-datareader to get prices from google.com.
 """
 
-# Line below depends on shell command:
+# pandas_datareader depends on shell command:
 # conda install pandas-datareader
 
 import pandas_datareader as pdr
 import datetime
+import pdb
 
-start  = datetime.datetime(2016, 1, 1)
-end    = datetime.datetime(2016, 12, 31)
-prices = pdr.DataReader("IBM", 'google', start, end)
-print(prices.head())
+# I should stop the script here and start the debugger:
+pdb.set_trace()
+
+start_dt  = datetime.datetime(2016,  1,  1)
+end_dt    = datetime.datetime(2026, 12, 31)
+prices_df = pdr.DataReader('IBM', 'google', start_dt, end_dt)
+print(prices_df.head())
+
+# I should write the prices to a CSV file in /tmp:
+prices_df.to_csv('/tmp/prices_ibm.csv', float_format='%4.3f')
+
+# I should end the script with a simple string which might be a
+# convenient breakpoint for the debugger:
+
 'bye'
