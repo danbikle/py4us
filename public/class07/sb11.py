0def423d43773a2c7e4010702f5601b0bdcbef65
Dan Bikle <bikle@bikle.com>
2017-07-16 22:05:06 -0700

    more

--- public/class07/sb11.py
+++ public/class07/sb11.py
@@ -12,7 +12,6 @@ import seaborn as sns
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-np.random.seed(sum(map(ord, "aesthetics")))
 
 def sinplot(flip=1):
     x = np.linspace(0, 14, 100)
