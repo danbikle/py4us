f2871671c4f280e71c1523842999b447a8957d8c
Dan Bikle <danbikle@outlook.com>
2017-12-17 23:34:53 -0800

    more

---
+++ public/class02/interesting.py
@@ -0,0 +1,48 @@
+"""
+interesting.py
+
+This script shows some syntax-answers for the Class02 'Interesting' Lab.
+
+Ref:
+http://py4.us/cclasses/class02#other
+
+Demo:
+git clone https://github.com/danbikle/py4us
+cd py4us/public/class02/
+~/anaconda3/bin/python interesting.py
+"""
+
+import pdb
+import pandas as pd
+import numpy as np
+import io
+
+# Create a Tuple from a List
+# Create a Set from a List
+# Use a Set to get unique items from a List
+# Create a Pandas Series from a List
+# Create a Pandas DataFrame, then add a List to it
+# Create a NumPy Array from a List
+# Write a simple example of the zip() function
+# Create a dictionary, then create a List from its keys
+# Create a dictionary, then create a List from its values
+# Find all values in list1 which are in list2
+# Find all values in list1 which are not in list2
+# Reverse the order of a List
+# Sort the items in a List
+# Loop through lines in a text file to create List of strings
+# Write a simple example of using loc[] with a Pandas Series
+# Write a simple example of using iloc[] with a Pandas Series
+# Describe how loc is different than iloc
+# Write an example of the filter() function
+# Write an example of the map() function
+# Write an example of the reduce() function
+# Write an example of the Pandas apply() function
+# With one line of syntax, square all numbers in a List
+# With one line of syntax, read py4.us/gspc.csv into a DataFrame
+# Use the CSV module to read a CSV file into a list of lists
+# Use modules: CSV, io, and Requests to read py4.us/gspc.csv into a List of Lists
+# Read all filenames from /etc-folder into a list
+
+  
+
