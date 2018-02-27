19ef04a497852ad689114e7fcccec2b260b89ade
Dan Bikle <danbikle@outlook.com>
2018-02-26 17:07:52 -0800

    more

--- public/class01/plot1.py
+++ public/class01/plot1.py
@@ -15,7 +15,7 @@ import pandas_datareader as pdr
 # I should get the prices:
 start_dt   = datetime.datetime(2016,  1,  1)
 end_dt     = datetime.datetime(2026, 12, 31)
-prices1_df = pdr.DataReader('IBM', 'yahoo', start_dt, end_dt)
+prices1_df = pdr.DataReader('IBM', 'google', start_dt, end_dt)
 # I should get 2016-11 data:
 pred_sr    = prices1_df.index > '2016-11'
 prices2_df = prices1_df.copy()[['Close']][pred_sr]
